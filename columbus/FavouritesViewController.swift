83407a2c90e3d20ecffcb1a0de0679e9a89bbea5
VIctor Zhang <vic@tumblr.com>
2015-10-11 14:32:23 -0400

    created views

---
+++ columbus/FavouritesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FavouritesViewController.swift
+//  columbus
+//
+//  Created by Victor Zhang on 10/11/15.
+//  Copyright © 2015 Victor Zhang. All rights reserved.
+//
+
+import UIKit
+
+class FavouritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
